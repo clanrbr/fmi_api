0c38f125e4428d90e566e5d1bf4b7c83cebf9146
clanrbr <clan_rbr@yahoo.com>
2013-03-25 00:30:18 +0200

    Course
    Course _ addeed

--- index.php
+++ index.php
@@ -7,10 +7,29 @@ require  'Redbeanphp/rb.php';
 R::setup('mysql:host=localhost;dbname=testdb','root','');
 R::freeze(true);
 
+
+$groupMap = array(
+	"Д" => "DID",
+	"Др." => "OTHR",
+	"КП" => "CSP" /*ComputerScience - Practicum*/,
+	"М" => "MAT",
+	"ОКН" => "CSF" /*CS Fundamentals*/,
+	"ПМ" => "APM" /*APPLIED MATH*/,
+	"С" => "SEM" /*Seminars*/,
+	"Ст" => "STAT" /*Statistics*/,
+	"Х" => "HUM" /*Humanitarian*/,
+	"ЯКН" => "CSC" /*CS Core*/,
+	"И" => "INF" /*informatics*/,
+	"ПМ / Ст" => array("APM", "STAT"), /*wtf fmi*/
+	"ПМ/Ст" => array("APM", "STAT"), /*wtf fmi x2*/
+	"ОКН/Ст" => array("CSF", "STAT") /*wtf fmi x3*/
+);
+
 $app = new \Slim\Slim();
+
 $app->get('/programs', function () use ($app) {
 	try {
-		$programs=R::getAll('select * from bachelor_programmes');
+		$programs=R::getAll('SELECT * FROM bachelor_programmes');
 		if ($programs) {
 			$app->response()->header('Content-Type', 'application/json');
 			echo json_encode($programs,JSON_UNESCAPED_UNICODE);
@@ -48,5 +67,51 @@ $app->get('/programs/:id', function ($id) use ($app) {
 			  $app->response()->header('X-Status-Reason', $e->getMessage());
 	}
 });
+
+$app->get('/courses', function () use ($app) {
+	try {
+		$courses = R::getAll("SELECT courses.course_id, courses.course_name, courses.group, courses.credits, courses.semester, bachelor_programmes.programme_name, courses.year FROM courses LEFT JOIN bachelor_programmes on courses.programme_id=bachelor_programmes.programme_id");
+		if ($courses) {
+			$app->response()->header('Content-Type', 'application/json');
+			echo json_encode($courses  , JSON_UNESCAPED_UNICODE );
+		} 
+		else {
+			throw new ResourceNotFoundException();
+		}
+	}
+	catch (ResourceNotFoundException $e) {
+		$app->response()->status(404);
+	}
+	catch (Exception $e) {
+			  $app->response()->status(400);
+			  $app->response()->header('X-Status-Reason', $e->getMessage());
+	}
+});
+
+$app->get('/courses', function () use ($app) {
+	try {
+		$courses = R::getAll("SELECT courses.course_id, courses.course_name, courses.group, courses.credits, courses.semester, bachelor_programmes.programme_name, courses.year FROM courses LEFT JOIN bachelor_programmes on courses.programme_id=bachelor_programmes.programme_id");
+		if ($courses) {
+			$app->response()->header('Content-Type', 'application/json');
+			echo json_encode($courses  , JSON_UNESCAPED_UNICODE );
+		} 
+		else {
+			throw new ResourceNotFoundException();
+		}
+	}
+	catch (ResourceNotFoundException $e) {
+		$app->response()->status(404);
+	}
+	catch (Exception $e) {
+			  $app->response()->status(400);
+			  $app->response()->header('X-Status-Reason', $e->getMessage());
+	}
+});
+
+// http://alexbilbie.com/2013/02/securing-your-api-with-oauth-2/
+// http://help.slimframework.com/discussions/questions/230-can-i-have-a-get-request-with-variable-number-of-parameters-in-the-url
+// http://nesbot.com/2012/6/18/slim-wildcard-routes-via-route-middleware
+// http://linkey.blogs.lincoln.ac.uk/tag/access-token/
+
 R::close();
 $app->run();
