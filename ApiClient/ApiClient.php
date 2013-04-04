7db6e649f3c4cb2eac9485ca678efe9b6c219375
clanrbr <clan_rbr@yahoo.com>
2013-04-04 22:58:17 +0300

    api program id added

--- ApiClient/ApiClient.php
+++ ApiClient/ApiClient.php
@@ -4,7 +4,7 @@
 		public $token;
 		private $url="http://localhost/api/index.php/";
 
--		public function __construct($token="") {
+		public function __construct($token="") {
 			$this->token = $token;
 		}
 		
@@ -15,6 +15,25 @@
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 			$output = curl_exec($ch);
 			return $output;
+		}
+		
+		private function _get_all_program() {
+			$params=func_get_args();
+			$ch = array_shift($params);
+			$id="";
+			if ( isset($params) )
+			  $id=array_shift($params);
+				
+			curl_setopt($ch, CURLOPT_URL, $this->url."/".$this->token."/program/".$id); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			$output = curl_exec($ch);
+			$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			if ($status_code==404) {
+				return json_encode(array('error'=>'Missing parameter ID'),JSON_UNESCAPED_UNICODE);
+			}
+			else {
+				return $output;
+			}
 		}
 		
 		
@@ -29,4 +48,7 @@
 
 	$cl= new ApiClient("Bshkpc5KWESLAZQGx");
 	$cool=$cl->get_all_programs('1','2');	
+	$cool1=$cl->get_all_program();	
+	echo $cool1;
+	// var_dump($cool1);
 ?>
