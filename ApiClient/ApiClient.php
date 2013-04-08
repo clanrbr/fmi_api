802e5a8509b228d050508181863bd59728b09946
clanrbr <clan_rbr@yahoo.com>
2013-04-09 00:27:38 +0300

    removed some unused functions

--- ApiClient/ApiClient.php
+++ ApiClient/ApiClient.php
@@ -8,31 +8,6 @@
 		public function __construct($token="") {
 			$this->token = $token;
 		}
-        
-        function myErrorHandler($errno, $errstr, $errfile, $errline) {
-            // if (!(error_reporting() & $errno)) {
-                // return;
-            // }
-            switch ($errno) {
-                case E_USER_ERROR:
-                    echo "ERROR : [$errno] $errstr on line $errline in file $errfile\n";
-                    exit(1);
-                    break;
-
-                case E_USER_WARNING:
-                    echo "WARNING [$errno] $errstr\n";
-                    break;
-
-                case E_USER_NOTICE:
-                    echo "NOTICE [$errno] $errstr\n";
-                    break;
-
-                default:
-                    echo "Unknown error type: [$errno] $errstr\n";
-                    break;
-            }
-            return true;
-        }
 		
 		private function _get_all_programs() {
 			$data=[];
