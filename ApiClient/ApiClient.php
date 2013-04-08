9fb9c51cfce4c72ae038a6d8be086ffb7da3f9c6
clanrbr <clan_rbr@yahoo.com>
2013-04-09 00:25:56 +0300

    fixed style

--- ApiClient/ApiClient.php
+++ ApiClient/ApiClient.php
@@ -35,75 +35,69 @@
         }
 		
 		private function _get_all_programs() {
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/programs"); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-			return $output;
+			$data=[];
+			$data['data']=$this->url."/".$this->token."/programs"; 
+			return $data;
 		}
 		
 		private function _get_program_by_id($id=0) {
+			$data=[];
 			if ( empty($id) ) {
-                trigger_error("Missing parameter id in get_program_by_id", E_USER_ERROR);
-                return;
+                $data['error']="Missing parameter id in get_program_by_id";
+                return $data;
             }
                 
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/program/".$id); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/program/".$id;
+            return $data;
 		}
         
         private function _gel_all_teachers() {
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/teachers"); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-			return $output;
+			$data=[];
+            $data['data']=$this->url."/".$this->token."/teachers";
+			return $data;
         }
         
         private function _get_teachers_by_department($department="") {
+			$data=[];
 			if ( empty($department) ) {
-				trigger_error("Missing parameter department in get_teachers_by_department", E_USER_ERROR);
-				return;
+				$data['data']="Missing parameter department in get_teachers_by_department";
+				return $data;
 			}
 
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/teachers/department/".$department); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/teachers/department/".$department;
+            return $data;
         }
 		
         private function _get_teachers_by_position($position="") {
+			$data=[];
 			if ( empty($position) ) {
-				trigger_error("Missing parameter position in get_teachers_by_position", E_USER_ERROR);
-				return;
+				$data['error']="Missing parameter position in get_teachers_by_position";
+				return $data;
 			}
 
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/teachers/position/".urlencode($position)); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/teachers/position/".urlencode($position);
+            return $data;
         }
         
         private function _get_teacher_by_id($id=0) {
+			$data=[];
 			if ( empty($id) ) {
-                trigger_error("Missing parameter id in get_teacher_by_id", E_USER_ERROR);
-                return;
+                $data['error']="Missing parameter id in get_teacher_by_id";
+                return $data;
             }
 
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/teacher/".$id); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/teacher/".$id; 
+            return $data;
 		}
         
         private function _get_all_semesters() {
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/semesters"); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-			return $output;
+			$data=[];
+            $data['data']=$this->url."/".$this->token."/semesters";
+			return $data;
         }
         
         private function _get_semesters_by_filter($params) {
+			$data=[];
 			$urlextend="";
 			if ( isset($params['season']) )
 			  $urlextend.="/".$params['season'];
@@ -112,105 +106,94 @@
 			if ( isset($params['end_year']) )
 			  $urlextend.="/".$params['end_year'];
             
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/semester_filter".$urlextend); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/semester_filter".$urlextend;
+            return $data;
         }
 
 		private function _get_semesters_by_season($season) {
+			$data=[];
 			if ( empty($season) ) {
-                trigger_error("Missing parameter season in get_semesters_by_season", E_USER_ERROR);
-                return;
+                $data['error']="Missing parameter season in get_semesters_by_season";
+                return $data;
             }
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/semesters/season/".$season); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/semesters/season/".$season; 
+            return $data;
 		}
 		
 		private function _get_semesters_by_start_year($start_year=0) {
+			$data=[];
 			if ( empty($start_year) ) {
-				trigger_error("Missing parameter start_year in get_semesters_by_start_year", E_USER_ERROR);
-                return;
+				$data['error']="Missing parameter start_year in get_semesters_by_start_year";
+                return $data;
 			}
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/semesters/start/".$start_year); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/semesters/start/".$start_year; 
+            return $data;
 		}
 		
 		private function _get_semesters_by_end_year($end_year=0) {
+			$data=[];
 			if ( empty($end_year) ) {
-				trigger_error("Missing parameter end_year in get_semesters_by_end_year", E_USER_ERROR);
-                return;
+				$data['error']="Missing parameter end_year in get_semesters_by_end_year";
+                return $data;
 			}
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/semesters/end/".$end_year); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/semesters/end/".$end_year;
+            return $data;
 		}
 		
 		private function _get_all_courses() {
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses"); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($this->ch);
-            return $output;
+			$data=[];
+			$data['data']=$this->url."/".$this->token."/courses";
+            return $data;
 		}
 		
 		private function _get_courses_by_id($id=0) {
+			$data=[];
 			if ( empty($id) ) {
-                trigger_error("Missing parameter id in get_teacher_by_id", E_USER_ERROR);
-                return;
+				$data['error']="Missing parameter id in get_teacher_by_id";
+                return $data;
             }
-
-			curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/course/".$id); 
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-			$output = curl_exec($this->ch);
-            return $output;
+			$data['data']=$this->url."/".$this->token."/course/".$id;
+            return $data;
 		}
-
-        private function _get_courses_by_semester_year($year=0) {
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses/year/".$year); 
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-            $output = curl_exec($this->ch);
-            return $output;
+		
+		private function _get_courses_by_semester_year($year=0) {
+			$data=[];
+            $data['data']=$this->url."/".$this->token."/courses/year/".$year); 
+            return $data;
         }
 
         private function _get_courses_by_semester($semester=0) {
+			$data=[];
             if ( empty($semester) ) {
-                trigger_error("Missing parameter semester in get_courses_by_semester", E_USER_ERROR);
-                return;
+                $data['data']="Missing parameter semester in get_courses_by_semester";
+                return $data;
             }
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses/semester/".$semester); 
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-            $output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/courses/semester/".$semester; 
+            return $data;
         }
 
         private function _get_courses_by_credits($credits="") {
+			$data=[];
             if ( empty($credits) ) {
-                trigger_error("Missing parameter credits in get_courses_by_credits", E_USER_ERROR);
-                return;
+                 $data['error']="Missing parameter credits in get_courses_by_credits";
+                return $data;
             }
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses/credits/".$credits); 
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-            $output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/courses/credits/".$credits; 
+            return $data;
         }
 
         private function _get_courses_by_group($group="") {
+			$data=[];
             if ( empty($group) ) {
-                trigger_error("Missing parameter group in get_courses_by_group", E_USER_ERROR);
-                return;
+                $data['error']="Missing parameter group in get_courses_by_group";
+                return $data;
             }
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses/group/".$group); 
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1); 
-            $output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/courses/group/".$group; 
+            return $data;
         }
 
         private function _get_program_by_filter($filter) {
+			$data=[];
             $urlextend="";
             if ( isset($filter['year']) )
               $urlextend.="/".$filter['year'];
@@ -219,17 +202,20 @@
             if ( isset($filter['semester']) )
               $urlextend.="/".$filter['semester'];
             
-            curl_setopt($this->ch, CURLOPT_URL, $this->url."/".$this->token."/courses/program".$urlextend); 
-            curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-            $output = curl_exec($this->ch);
-            return $output;
+            $data['data']=$this->url."/".$this->token."/courses/program".$urlextend; 
+            return $data;
         }
-
-        // courses/program(/:year(/:program_id(/:semester)))
 
 		public function __call($func, $params=array()) {
 			$this->ch = curl_init();
-			$output = call_user_func_array(array($this,'_'.$func),$params);
+			$result = call_user_func_array(array($this,'_'.$func),$params);
+			if (isset($result['error']) ) {
+				return json_encode($result , JSON_UNESCAPED_UNICODE );
+			}
+			
+			curl_setopt($this->ch, CURLOPT_URL, $result['data']);
+			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+			$output = curl_exec($this->ch);
 			curl_close($this->ch);
 			return $output;
 		}
@@ -239,8 +225,8 @@
 	$cool=$cl->get_all_programs();
 	$cool1=$cl->get_program_by_id(1);	
     $cool2=$cl->gel_all_teachers();
-    $cool3=$cl->get_teachers_by_department("ÑÒ");
-    $cool4=$cl->get_teachers_by_position("ãëàâåí àñèñòåíò");
+    $cool3=$cl->get_teachers_by_department("Ð¡Ð¢");
+    $cool4=$cl->get_teachers_by_position("Ð³Ð»Ð°Ð²ÐµÐ½ Ð°ÑÐ¸ÑÑ‚ÐµÐ½Ñ‚");
     $cool5=$cl->get_teacher_by_id(1);
     $cool6=$cl->get_all_semesters();
     $cool7=$cl->get_semesters_by_filter(array("season"=>"summer","start_year"=>2012,"end_year"=>2013));
@@ -249,11 +235,10 @@
 	$cool10=$cl->get_semesters_by_end_year(2013);
 	$cool11=$cl->get_all_courses();
 	$cool12=$cl->get_courses_by_id(4);
-    $cool13=$cl->get_courses_by_semester_year(0);
+	$cool13=$cl->get_courses_by_semester_year(0);
     $cool14=$cl->get_courses_by_semester(1);
     $cool15=$cl->get_courses_by_credits(">7");
-    $cool16=$cl->get_courses_by_group("Ä");
+    $cool16=$cl->get_courses_by_group("Ã„");
     $cool17=$cl->get_program_by_filter(array("year"=>"0","program_id"=>9,"semester"=>1));
-    // "program_id"=>"1","semester"=>1
-	echo $cool17;
+	echo $cool12;
 ?>
